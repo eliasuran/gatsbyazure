9ce3e1671fcc234cc446ccfc8fb413a35c07bfc2
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-18 10:57:45 +0100

    init

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import { Link, graphql } from "gatsby"
 
 export default ({ data }) => (
   <React.Fragment>
+    <h1>bruh</h1>
     <h2>{data.site.siteMetadata.title}</h2>
     <div>{data.site.siteMetadata.description}</div>
     <Link to="/">Back to home</Link>
